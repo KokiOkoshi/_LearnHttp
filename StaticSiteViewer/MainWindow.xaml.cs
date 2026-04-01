9bc0d47a4de837a45070bd556f7e9fac973677bf
Koki Okoshi <okoshi_koki@hotmail.co.jp>
2026-04-01 23:39:30 +0900

    WebView2でAstroの静的サイトを表示 ・一旦CoreWebView2.SetVirtualHostNameToFolderMappingメソッドで対応してみたが、index.htmlの表示させ方が無理やり ・ページ内リンク非対応 ・404ページ非対応

--- StaticSiteViewer/MainWindow.xaml.cs
+++ StaticSiteViewer/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Wpf;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,26 @@ namespace StaticSiteViewer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void WebView2_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            if (sender is not WebView2 webView2) return;
+            if (!e.IsSuccess) return;
+
+            webView2.CoreWebView2.SetVirtualHostNameToFolderMapping("localhost", "dist", Microsoft.Web.WebView2.Core.CoreWebView2HostResourceAccessKind.Allow);
+            webView2.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+        }
+
+        private void CoreWebView2_NavigationStarting(object? sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs e)
+        {
+            if (sender is not CoreWebView2 coreWebView2) return;
+            if (!Uri.TryCreate(e.Uri, UriKind.Absolute, out var uri)) return;
+            if (uri.Host != "localhost") return;
+            if (!e.Uri.EndsWith('/')) return;
+
+            e.Cancel = true;
+            coreWebView2.Navigate(e.Uri + "index.html");
         }
     }
 }
